a33f9b05ec596d03441dede0670703de1aefba08
stonegrillrestaurant <ninoxx@gmail.com>
2025-07-27 01:08:57 +0800

    ydy

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,29 +1,21 @@
-const cacheName = 'stonegrill-cache-v1';
+const cacheName = 'stonegrill-v2';
 const assets = [
-  '/',
-  '/delicious/index.html',
-  '/assets/img/menuF.jpg',
-  '/assets/img/menu-2.png',
-  '/assets/img/apple-touch-icon180x180.png',
-  '/assets/img/favicon-32x32.png',
-  '/assets/img/favicon-16x16.png',
-  '/site.webmanifest'
+  'index.html',
+  'offline.html',
+  'assets/css/main.css',
+  'assets/img/apple-touch-icon180x180.png'
 ];
 
-// Install
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-// Fetch
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    fetch(e.request).catch(() => caches.match(e.request).then(res => {
+      return res || caches.match('offline.html');
+    }))
   );
 });
