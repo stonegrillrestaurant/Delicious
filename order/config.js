d08c2a0946f29bcc985b5bfc1632ef399daf3374
stonegrillrestaurant <ninoxx@gmail.com>
2025-08-09 00:59:51 +0800

    Update config.js

--- order/config.js
+++ order/config.js
@@ -3,7 +3,6 @@ window.APP_CONFIG = {
   TELEGRAM_CHAT_ID: "-1002531095369",
   SHEETS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyKlYNy78JDPvS_Dl65sHT24T5FE33XBWB-uo3rrafz3aCP5RuihGnJgeoq-NRc46iwsA/exec",
   GCASH_QR_PATH: "assets/qr/gcash.png",
-
   SHOP_NAME: "Stone Grill Restaurant",
   PHONE: "053 568 0539",
   HOURS: "Daily 10:00 AM – 9:00 PM",
